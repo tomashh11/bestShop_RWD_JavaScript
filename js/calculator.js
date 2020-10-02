307ab8538cf5d4f66f8e78556d54c4223683080c
Tomasz Wroński <tomashh11@gmail.com>
2020-10-02 14:05:10 +0200

    ver 1.1 code optimization

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const inputQuantity = document.querySelector("#quantity");
-    const divActiveQuantity = document.querySelector(".nonActiveQuantity");
-    const inputMonthsOrder = document.querySelector("#monthsOrder");
-    const divActiveMonthsOrder = document.querySelector(".nonActiveMonthsOrder");
-    const divActiveSelectPackages = document.querySelector(".nonActiveSelectPackages");
-    const sectionSelect = document.querySelector("#packages");
-    const inputAccounting = document.querySelector(".inputAccounting");
-    const divAccounting = document.querySelector(".nonActiveInputAccounting");
-    const inputRental = document.querySelector(".inputRental");
-    const divRental = document.querySelector(".nonActiveInputRental");
+    const quantity = document.querySelector("#quantity");
+    const nonActiveQuantity = document.querySelector(".nonActiveQuantity");
+    const monthsOrder = document.querySelector("#monthsOrder");
+    const nonActiveMonthsOrder = document.querySelector(".nonActiveMonthsOrder");
+    const nonActivePackages = document.querySelector(".nonActivePackages");
+    const packages = document.querySelector("#packages");
+    const accounting = document.querySelector(".accounting");
+    const nonActiveAccounting = document.querySelector(".nonActiveAccounting");
+    const rental = document.querySelector(".rental");
+    const nonActiveRental = document.querySelector(".nonActiveRental");
 
     const quantityConverter = 0.5;
     const monthsOrderMap = new Map([['Basic', 10], ['Professional', 30], ['Premium', 60]]);
@@ -20,78 +20,77 @@ document.addEventListener('DOMContentLoaded', function () {
     let accountingValueCounter = 0;
     let rentalValueCounter = 0;
 
-    function handleQuantityInput(e) {
-        quantityCounter = e.target.value * quantityConverter;
-        divActiveQuantity.classList.add('activeQuantity');
-        divActiveQuantity.innerHTML = `
-        <span class="products">Products</span>
-        <span class="productsNumbers">${inputQuantity.value} * ${quantityConverter}</span>
-        <span class="productsSum">$${quantityCounter}</span>
-        `;
-        sumTotal();
-    }
+    quantity.oninput = handleQuantityInput;
+    monthsOrder.oninput = handleMonthsOrderInput;
 
-    inputQuantity.oninput = handleQuantityInput;
-
-    function handleMonthsOrderInput(e) {
-        monthsOrderCounter = e.target.value * monthsOrderConverter;
-        divActiveMonthsOrder.classList.add('activeMonthsOrder');
-        divActiveMonthsOrder.innerHTML = `
-            <span class="orders">Orders</span>
-            <span class="ordersNumbers">${inputMonthsOrder.value} * ${monthsOrderConverter}</span>
-            <span class="ordersSum">$${monthsOrderCounter}</span>
-            `;
-        sumTotal();
-    }
-
-    inputMonthsOrder.oninput = handleMonthsOrderInput;
-
-    sectionSelect.addEventListener('change', function () {
+    packages.addEventListener('change', function () {
         packagesCounter = monthsOrderMap.get(this.value);
-        divActiveSelectPackages.classList.add('activeSelectPackages');
-        divActiveSelectPackages.innerHTML = `
-        <span class="spanSelect">Package</span>
-        <span class="selectPackage">${this.value}</span>
-        <span class="selectValue">$${packagesCounter}</span>
+        nonActivePackages.classList.add('activePackages');
+        nonActivePackages.innerHTML = `
+        <span class="packageTitle">Package</span>
+        <span class="chosenPackage">${this.value}</span>
+        <span class="packageValue">$${packagesCounter}</span>
         `;
         sumTotal();
     });
 
-    inputAccounting.addEventListener('change', function () {
+    accounting.addEventListener('change', function () {
         if (this.checked) {
             accountingValueCounter = 20;
-            divAccounting.classList.add('activeInputAccounting');
-            divAccounting.innerHTML = `
-            <span class="spanAccounting">Accounting</span>
-            <span class="spanAccountingValue">$${accountingValueCounter}</span>
+            nonActiveAccounting.classList.add('activeAccounting');
+            nonActiveAccounting.innerHTML = `
+            <span class="accountingTitle">Accounting</span>
+            <span class="accountingValue">$${accountingValueCounter}</span>
             `;
             sumTotal();
         } else {
-            divAccounting.classList.remove('activeInputAccounting');
+            nonActiveAccounting.classList.remove('activeAccounting');
             accountingValueCounter = 0;
             sumTotal();
         }
     });
 
-    inputRental.addEventListener('change', function () {
+    rental.addEventListener('change', function () {
         if (this.checked) {
             rentalValueCounter = 5;
-            divRental.classList.add('activeInputRental');
-            divRental.innerHTML = `
-            <span class="spanRental">Terminal</span>
-            <span class="spanRentalValue">$${rentalValueCounter}</span>
+            nonActiveRental.classList.add('activeRental');
+            nonActiveRental.innerHTML = `
+            <span class="rentalTitle">Terminal</span>
+            <span class="rentalValue">$${rentalValueCounter}</span>
             `;
             sumTotal();
         } else {
-            divRental.classList.remove('activeInputRental');
+            nonActiveRental.classList.remove('activeRental');
             rentalValueCounter = 0;
             sumTotal();
         }
     });
 
+    function handleQuantityInput(e) {
+        quantityCounter = e.target.value * quantityConverter;
+        nonActiveQuantity.classList.add('activeQuantity');
+        nonActiveQuantity.innerHTML = `
+        <span class="productsTitle">Products</span>
+        <span class="productsNumbers">${quantity.value} * ${quantityConverter}</span>
+        <span class="productsSum">$${quantityCounter}</span>
+        `;
+        sumTotal();
+    }
+
+    function handleMonthsOrderInput(e) {
+        monthsOrderCounter = e.target.value * monthsOrderConverter;
+        nonActiveMonthsOrder.classList.add('activeMonthsOrder');
+        nonActiveMonthsOrder.innerHTML = `
+            <span class="orderTitle">Orders</span>
+            <span class="orderNumbers">${monthsOrder.value} * ${monthsOrderConverter}</span>
+            <span class="orderSum">$${monthsOrderCounter}</span>
+            `;
+        sumTotal();
+    }
+
     function sumTotal() {
         let sum = quantityCounter + monthsOrderCounter + packagesCounter + accountingValueCounter + rentalValueCounter;
-        const btnTotal = document.querySelector(".btnTotal");
+        const btnTotal = document.querySelector(".total");
         btnTotal.innerText = `$ ${sum}`;
     }
 
--- js/hamburger.js
+++ js/hamburger.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const btnHamburger = document.querySelector(".hamburger");
-    const headerNavUl = document.querySelector(".header_nav_ul");
-    const hamburgerClose = document.querySelector(".hamburgerClose");
+    const hamburger = document.querySelector(".header-hamburger");
+    const headerNavUl = document.querySelector(".header-nav-ul");
+    const hamburgerClose = document.querySelector(".header-hamburger-close");
 
-    btnHamburger.addEventListener("click", function () {
+    hamburger.addEventListener("click", () => {
         headerNavUl.classList.add("show");
         hamburgerClose.classList.add("on");
-        btnHamburger.classList.add("off");
+        hamburger.classList.add("off");
     });
 
     hamburgerClose.addEventListener("click", () => {
         headerNavUl.classList.remove("show");
         hamburgerClose.classList.remove("on");
-        btnHamburger.classList.remove("off");
+        hamburger.classList.remove("off");
     })
 
 });
